831f6ebb73bec923455d78d69ae0a2956e7b9c3f
IgnacioDemarcico <115153358+IgnacioDemarcico@users.noreply.github.com>
2023-05-05 10:40:42 -0300

    animacion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,11 +18,17 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey("left"))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-300f * Time.deltaTime,0));
+	        gameObject.GetComponent<Animator>().SetBool("movimiento",true);
         }
         if(Input.GetKey("right"))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(300f * Time.deltaTime,0));
+            gameObject.GetComponent<Animator>().SetBool("movimiento",true);
         }
+	if(!Input.GetKey("left") && !Input.GetKey("right"))
+	{
+        gameObject.GetComponent<Animator>().SetBool("movimiento",false);
+    }
 
         Salto();
     }
