50fc521405c6946f38efc35ff011499c7924373d
TeiToTeo <teo.e.navarro@gmail.com>
2023-05-05 08:55:54 -0300

    ENEMIGOOOO y ataque

---
+++ Assets/AtqMelee.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtqMelee : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform controladorMelee;
+    [SerializeField] private float radioGolpe;
+    [SerializeField] private float dañoGolpe;
+}
