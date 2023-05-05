aca2c19a3cca3f026e1ceb7a20e4e9b63f3a43b9
TeiToTeo <teo.e.navarro@gmail.com>
2023-05-05 11:04:03 -0300

    Enemigo y ataque un poco mas avanzado

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    [SerializeField] private float vida;
+    //private Animator animator
+    /*private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }*/
+    public void TomarDanio(float danio)
+    {
+        vida -= danio;
+        if(vida <= 0)
+        {
+            Muerte();
+        }
+    }
+    private void OnCollisionEnter2D(collision2d other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.Morir();
+            other.gameObject.Getcomponent<PlayerController>().AplicarGolpe();
+        }
+    }
+    /*private void Muerte()
+    {
+        animator.Set.Trigger("Muerte");
+    }*/
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     bool canJump;
-    // Start is called before the first frame update
+    private bool puedeMoverse; //va a servir para cuando un enemigo lo golpee
     void Start()
     {
         
