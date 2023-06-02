efeffc57df7b7fa24c8bbb8dc6905e6211077ebf
TeiToTeo <teo.e.navarro@gmail.com>
2023-06-02 09:05:53 -0300

    Chequar knockback

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -1,30 +1,41 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movete : MonoBehaviour
+public class Movimiento : MonoBehaviour
 {
     public float velocidad = 5f;
-    public float knockBackLength, knockBackForce;
-    private float knockBackCounter;
-    public static PlayerController instance;
+    public float fuerzaSalto = 50f;
+    private bool enSuelo = true;
 
     private Rigidbody2D rb;
+    public float KnockBackLength, knockBackForce;
+    private float knockBackCounter;
+    public static Movimiento instance;
 
-    /*private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-    public void Awake()
+
+    private void Awake()
     {
         instance = this;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        
-        if(knockBackCounter <= 0)
+        if (knockBackCounter <= 0)
         {
+            if (Input.GetKey("a"))
+            {
+                gameObject.GetComponent<Animator>().SetBool("movimiento", true);
+            }
+            if (Input.GetKey("d"))
+            {
+                gameObject.GetComponent<Animator>().SetBool("movimiento", true);
+            }
+            if (!Input.GetKey("a") && !Input.GetKey("d"))
+            {
+                gameObject.GetComponent<Animator>().SetBool("movimiento", false);
+            }
             float movimientoHorizontal = Input.GetAxis("Horizontal");
 
             // Calcula la velocidad del movimiento
@@ -32,15 +43,38 @@ public class Movete : MonoBehaviour
 
             // Aplica la velocidad al Rigidbody2D
             rb.velocity = velocidadMovimiento;
+
+            if (Input.GetKeyDown(KeyCode.Space) && enSuelo)
+            {
+                Saltar();
+            }
         }
         else
         {
             knockBackCounter -= Time.deltaTime;
         }
-    }
-    public void Knockback()
-    {
-        knockBackCounter = knockBackLength;
 
     }
-}*/
+
+    private void Saltar()
+    {
+        // Aplica una fuerza vertical al Rigidbody2D para el salto
+        rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+        enSuelo = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verifica si el personaje ha colisionado con el suelo
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            enSuelo = true;
+        }
+    }
+    public void KnockBack()
+    {
+        knockBackCounter = KnockBackLength;
+        rb.velocity = new Vector2(0f, knockBackForce);
+    }
+}
+
