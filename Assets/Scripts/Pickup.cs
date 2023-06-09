0f992d978a4250f93ef5e55fc82293e840392386
Teo Ezequiel Navarro <teo.e.navarro@gmail.com>
2023-06-09 10:38:47 -0300

    Moneda y checkpoint por la mitad

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    /*
+    public bool moneda;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(moneda)
+            {
+
+            }
+        }
+    }
+    */
+}
