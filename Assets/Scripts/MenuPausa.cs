86edbfe799f72c2901008101234c25f7918628a4
Teo Ezequiel Navarro <teo.e.navarro@gmail.com>
2023-09-22 09:56:17 -0300

    ANDA MENU DE PAUSA

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    public string levelSelect, mainMenu;
+    public GameObject menuPausa;
+    public bool enPausa;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Menu"))
+        {
+            PauseUnpause();
+        }
+    }
+    public void PauseUnpause()
+    {
+        if(enPausa)
+        {
+            enPausa = false;
+            menuPausa.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            enPausa = true;
+            menuPausa.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(levelSelect);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+}
